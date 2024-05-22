9264a8c51aa2a85d7ac12089c4f6d8333313bf31
Delower Hossain <delowerdipu420@gmail.com>
2024-05-22 14:37:43 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,5 @@ import 'Views/App/app.dart';
 
 //please use emulator for evaluation some packages don't support web.
 //I have provided device_preview
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  String storageLocation = (await getApplicationDocumentsDirectory()).path;
-  await FastCachedImageConfig.init(subDir: storageLocation, clearCacheAfter: const Duration(days: 2));
-  runApp(DevicePreview(builder: (context)=> const MyApp()));
+void main() { const MyApp();
 }
